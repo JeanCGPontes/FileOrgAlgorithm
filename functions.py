97591c13f0a0a569122a9d64fefe316f31668477
JeanCGPontes <jeanestudos.je@gmail.com>
2022-02-04 16:55:50 -0300

    function

--- functions.py
+++ functions.py
@@ -4,9 +4,7 @@ from exif import Image
 
 
 def getDateImage(image):
-    property_1 = Image(image).get("datetime_original")
-    property_2 = Image(image).get("datetime_digitized")
-    return [property_1[0:10].replace(":", "/"), property_2[0:10].replace(":", "/")]
-
-
-
+    property_1 = Image(image).datetime_original
+    property_2 = Image(image).datetime_digitized
+    property_3 = Image(image).datetime
+    return [property_1[0:10].replace(":", "/"), property_2[0:10].replace(":", "/"), property_3[0:10].replace(":", "/")]
