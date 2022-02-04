3e25f5d7733cbdad28a21678a67bae5ef592203b
JeanCGPontes <jeanestudos.je@gmail.com>
2022-02-04 09:56:35 -0300

    main

---
+++ main.py
@@ -0,0 +1,10 @@
+# Será usado para este programa o paradigma procedural.
+# SoftOrgDocuments — Software de Organização de Documentos.
+
+import functions
+
+image_1 = r"C:\Users\jeane\OneDrive\Imagens\Fotos do Computador da Thalia\20170630_114638.jpg"
+image_2 = r"C:\Users\jeane\OneDrive\Imagens\Fotos do Computador da Thalia\20170630_114710.jpg"
+
+print(functions.getDateImage(image_1))
+print(functions.getDateImage(image_2))
