3720c37a99d950325337c690773b76bd14f385ec
JeanCGPontes <jeanestudos.je@gmail.com>
2022-02-08 10:48:42 -0300

    main

--- main.py
+++ main.py
@@ -4,19 +4,9 @@
 import functions
 import os
 
-path = r"E:\Programação\Python\Projetos\SoftOrgDocuments\arquivo_com_fotos"
+path = r"C:\Users\jeane\OneDrive\Imagens\Fotos do Computador da Thalia"
 
 for a in range(0, len(os.listdir(path))):
     image_directory = fr"{path}\{os.listdir(path)[a]}"
-    print(functions.getDateImage(image_directory))
-
-
-"""for file in os.listdir(path):
-    print(functions.getDateImage(fr"{path}\{file}"))"""
-
-
-
-"""for directory, folders, files in os.walk(folder):
-    for file in files:
-        image_directory = os.path.join(os.path.realpath(directory), file)
-        print(functions.getDateImage(image_directory))"""
+    "print(functions.getDateImage(image_directory))"
+    print(functions.getFileType(image_directory))
