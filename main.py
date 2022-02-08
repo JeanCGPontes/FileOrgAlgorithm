67c941f668e438ab7da7af8c0951936fa125df54
JeanCGPontes <jeanestudos.je@gmail.com>
2022-02-08 10:57:58 -0300

    main

--- main.py
+++ main.py
@@ -8,5 +8,5 @@ path = r"C:\Users\jeane\OneDrive\Imagens\Fotos do Computador da Thalia"
 
 for a in range(0, len(os.listdir(path))):
     image_directory = fr"{path}\{os.listdir(path)[a]}"
-    "print(functions.getDateImage(image_directory))"
-    print(functions.getFileType(image_directory))
+    print(functions.getDateImage(image_directory))
+    "print(functions.getFileType(image_directory))"
