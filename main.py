29178ac922ab2badc3251ff1f8ee74ef99868205
JeanCGPontes <jeanestudos.je@gmail.com>
2022-02-09 10:56:45 -0300

    main

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 # Será usado para este programa o paradigma procedural.
 # SoftOrgDocuments — Software de Organização de Documentos.
+import os
+import shutil
 
 import functions
-import os
 
-path = r"C:\Users\jeane\OneDrive\Imagens\Fotos do Computador da Thalia"
+path = r"E:\Programação\Python\Projetos\SoftOrgDocuments\arquivo"
 
 for a in range(0, len(os.listdir(path))):
-    image_directory = fr"{path}\{os.listdir(path)[a]}"
-    print(functions.getDateImage(image_directory))
-    "print(functions.getFileType(image_directory))"
+    file = os.listdir(path)[a]
+    file_directory = fr"{path}\{file}"
+    file_type = functions.getFileType(file_directory)["classe"]
+
+    if not os.path.exists(fr"{path}\{file_type}"):
+        os.makedirs(fr"{path}\{file_type}")
+        shutil.move(src=file_directory, dst=fr"{path}\{file_type}\{file}")
+
+    else:
+        shutil.move(src=file_directory, dst=fr"{path}\{file_type}\{file}")
+
+
+"print(functions.getDateImage(image_directory))"
