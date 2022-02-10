df61d1fc8e6b31a630c8b7a019a83388aabcff02
JeanCGPontes <jeanestudos.je@gmail.com>
2022-02-10 09:41:13 -0300

    main

--- main.py
+++ main.py
@@ -7,16 +7,21 @@ import functions
 
 path = r"E:\Programação\Python\Projetos\SoftOrgDocuments\arquivo"
 
-for a in range(0, len(os.listdir(path))):
-    file = os.listdir(path)[a]
+for file in os.listdir(path):
     file_directory = fr"{path}\{file}"
     file_type = functions.getFileType(file_directory)["classe"]
 
+    if file_type is None:
+        continue
+
     if not os.path.exists(fr"{path}\{file_type}"):
+        print(fr"Criado {path}\{file_type}")
         os.makedirs(fr"{path}\{file_type}")
+        print(fr"{file} -> {path}\{file_type}")
         shutil.move(src=file_directory, dst=fr"{path}\{file_type}\{file}")
 
     else:
+        print(fr"{file} -> {path}\{file_type}")
         shutil.move(src=file_directory, dst=fr"{path}\{file_type}\{file}")
 
 
